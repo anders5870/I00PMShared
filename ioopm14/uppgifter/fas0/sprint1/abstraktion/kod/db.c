2843e28997112dd57c276c8c1d3b6b5880ce4421
Joel Berglund <joel.berglund.5241@student.uu.se>
2014-09-11 16:29:34 +0200

    Fixat query, håller på med update.

--- ioopm14/uppgifter/fas0/sprint1/abstraktion/kod/db.c
+++ ioopm14/uppgifter/fas0/sprint1/abstraktion/kod/db.c
@@ -15,9 +15,10 @@ void readline(char *dest, int n, FILE *source){
     dest[len-1] = '\0';
 }
 
-void query(char buffer, int n, Node cursor){
+void query(Node cursor){
   printf("Enter key: ");
-  readline(buffer, n, stdin);
+  char buffer[128];
+  readline(buffer, sizeof(buffer), stdin);
   puts("Searching database...\n");
   int found = 0;
   while(!found && cursor != NULL){
@@ -33,33 +34,35 @@ void query(char buffer, int n, Node cursor){
     printf("Could not find an entry matching key \"%s\"!\n", buffer);
   }
 }
-/*
-void update(char buffer, int n, Node *list){
-      printf("Enter key: ");
-      readline(buffer, n, stdin);
-      puts("Searching database...\n");
-      found = 0;
-      *list = cursor;
-      while(!found && cursor != NULL){
-        if(strcmp(buffer, cursor->key) == 0){
-          puts("Matching entry found:");
-          printf("key: %s\nvalue: %s\n\n", cursor->key, cursor->value);
-          found = 1;
-        }else{
-          cursor = cursor->next;
-        }
-      }
-      if(!found){
-        printf("Could not find an entry matching key \"%s\"!\n", buffer);
-      }else{
-        printf("Enter new value: ");
-        readline(buffer, n, stdin);
-        free(cursor->value);
-        cursor->value = malloc(strlen(buffer) + 1);
-        strcpy(cursor->value, buffer);
-        puts("Value inserted successfully!");
-      }
+
+void update(Node *list){
+  char buffer[128];
+  printf("Enter key: ");
+  readline(buffer, sizeof(buffer), stdin);
+  puts("Searching database...\n");
+  int found = 0;
+  Node cursor = *list;
+  while(!found && cursor != NULL){
+    if(strcmp(buffer, cursor->key) == 0){
+      puts("Matching entry found:");
+      printf("key: %s\nvalue: %s\n\n", cursor->key, cursor->value);
+      found = 1;
+    }else{
+      cursor = cursor->next;
+    }
+  }
+  if(!found){
+    printf("Could not find an entry matching key \"%s\"!\n", buffer);
+  }else{
+    printf("Enter new value: ");
+    readline(buffer, sizeof(buffer), stdin);
+    free(cursor->value);
+    cursor->value = malloc(strlen(buffer) + 1);
+    strcpy(cursor->value, buffer);
+    puts("Value inserted successfully!");
+  }
 }
+/*
 void update(char buffer, int n, Node *list){
   printf("Enter key: ");
   readline(buffer, n, stdin);
@@ -146,10 +149,10 @@ int main(int argc, char *argv[]){
   Node list = NULL;
    while(!(feof(database))){
     Node newNode = malloc(sizeof(struct node));
-    readline(buffer, 128, database);
+    readline(buffer, sizeof(buffer), database);
     newNode->key = malloc(strlen(buffer) + 1);
     strcpy(newNode->key, buffer);
-    readline(buffer, 128, database);
+    readline(buffer, sizeof(buffer), database);
     newNode->value = malloc(strlen(buffer) + 1);
     strcpy(newNode->value, buffer);
     newNode->next = list;
@@ -170,10 +173,10 @@ int main(int argc, char *argv[]){
     while(getchar() != '\n'); // Clear stdin
     switch(choice){
     case 1:
-      query(buffer, 128, list);
+      query(list);
       break;
     case 2:
-      //      update(buffer, 128, list);
+      update(list);
       break;
     case 3:
       //      insert(buffer, 128, list);
