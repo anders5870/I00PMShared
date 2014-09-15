f4c7d592c77e69e6a5aff0ee605bff32f7d2e8f1
Anders Ålander <anders58702014@Anders-Dator.lan>
2014-09-16 01:40:04 +0200

    +makefile,list.o,db.h

---
+++ ioopm14/uppgifter/fas0/sprint1/abstraktion/kod/db.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+  char *key;
+  char *value;
+  struct node *next;
+} *Node;
+
+
+typedef struct binTreeNode{
+  char *key;
+  char *value;
+  struct binTreeNode *lLeaf;
+  struct binTreeNode *rLeaf;
+}*BinTreeNode;
+
+void readline(char *dest, intn, FILE *source);
+//list specific
+void query(Node list);
+void update(Node list);
+void insert(Node *list);
+void delete(Node list);
+void print(Node list);
+
+//tree specific
+void addLeaf(BinTreeNode *root, BinTreeNode leaf);
+char *queryTree(BinTreeNode bt, char *match);
+void updateTree(BinTreeNode bt, char *match, char *newVal);
+void insertLeaf(BinTreeNode bt, char key, char value);
+void deleteNode(BinTreeNode bt, char *match);
+void printTree(BintTreeNode bt);
---
+++ ioopm14/uppgifter/fas0/sprint1/abstraktion/makefile
@@ -0,0 +1,11 @@
+db : db.c binTree.c list.c db.h
+	gcc -std=c99 -Wall -ggdb -o db db.c binTree.c list.c db.h
+
+binTree.o : binTree.c
+	gcc -std=c99 -Wall -ggdb -o binTree.o binTree.c
+
+list.o : list.c
+	gcc -std=c99 -Wall -ggdb -o list.o list.c
+
+db.h : db.h
+	gcc -std=c99 -Wall -ggdb -o db.h db.h
