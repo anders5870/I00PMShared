37f55db486dfbc7f0eb115891ca0cd44fb76194b
Anders Ålander <anders5870@yahoo.se>
2014-11-02 11:49:35 +0100

    added trafiksimulering eclipse workspace

---
+++ trafiksimulering/trafiksimulering/src/Car.java
@@ -0,0 +1,64 @@
+import java.util.Random;
+
+public class Car {
+
+    private int bornTime;
+    private int destination; // 1 för rakt fram, 2 för vänstersväng
+
+    private int currentPosition;
+    
+    public void step()
+    {
+    	bornTime = bornTime+1;
+    	// Uppdatera bilen ett tidssteg
+    }
+    //konstruktor
+    public Car(){
+    	bornTime = 0;
+    	destination = randInt(1,2);
+    	currentPosition = 0;
+    }
+    // get- oct set-metoder
+    //...
+    //get- metoder
+    public int getBornTime(){
+    	return this.bornTime;
+    }
+    public int getDestination(){
+    	return this.destination;
+    }
+    public int getCurrentPosition(){
+    	return this.currentPosition;
+    }    
+    
+    //set- metoder
+ 
+    public void setBornTime(int bornTime){
+    	this.bornTime =bornTime;
+    }
+    public void setDestination(int destination){
+    	this.destination =destination;
+    }
+    public void setCurrentPosition(int currentPosition){
+    	this.currentPosition =currentPosition;
+    }
+    
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+    
+    public String toString() {
+    	return "Car(bornTime=" + this.bornTime + ", destination=" + this.destination
+    			+", currentPosition=" + this.currentPosition + ")";
+    	}
+	
+}
