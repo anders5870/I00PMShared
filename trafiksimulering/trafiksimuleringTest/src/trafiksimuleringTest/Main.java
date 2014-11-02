9b68da0c77df678c6531ac76d0f04ca2ab4324b1
Bawer Calli <bawer_calli@hotmail.com>
2014-11-02 13:17:58 +0100

    a

--- trafiksimulering/trafiksimuleringTest/src/trafiksimuleringTest/Main.java
+++ trafiksimulering/trafiksimuleringTest/src/trafiksimuleringTest/Main.java
@@ -3,13 +3,20 @@ package trafiksimuleringTest;
 public class Main {
 
 	public static void main(String[] args) {
-	Car c1 = new Car();
+/*	Car c1 = new Car();
 	Car c2 = new Car();
 	Car c3 = new Car();
 	System.out.println(c1.toString());
 	System.out.println(c2.toString());
 	System.out.println(c3.toString());
 	
-	}
+	}*/
 
+			
+	Light1 = new light();
+	light2 = new light(50, 25);
+	System.out.println(Light1.toString());
+	System.out.println(Light2.toString());
+	
 }
+	
