e26a29f6191ee5abe258266cf3c5dc8a3a1e46da
Bawer Calli <baca0593@scheele.it.uu.se>
2014-11-02 12:17:48 +0100

    updated light class

--- trafiksimulering/trafiksimulering/src/Light.java
+++ trafiksimulering/trafiksimulering/src/Light.java
@@ -1,18 +1,48 @@
 public class Light {
-    private int period;
-    private int time;  // Intern klocka: 0, 1, ... period-1, 0, 1 ...
-    private int green; // Signalen grön när time<green 
+    private int period = 60;
+    private int time = 0 ;  // Intern klocka: 0, 1, ... period-1, 0, 1 ...
+    private int green = 30 ; // Signalen grön när time<green 
 
     public Light(int period, int green) {
-    	//...
-    	}
-
-    public void    step() { 
-       // Stegar fram klocka ett steg
+    	this.period = period;   
+    	this.green =  green;
+    }   	
+    
+    public Light(){
+    	period = 60; 
+    	green = 30;
+    }
+    
+    // get och set metoder för period och green
+    
+    public void setPeriod(int period) {
+    	this.period = period;
+    		}
+    	
+    public int getPeriod(){
+    	return this.period ;
+    }
+    	
+    public void setGreen(int green){
+    	this.green = green;
+    }
+    
+    public int getGreen(){
+    	return this.green;
+    }
+ 
+    
+    // Stegar fram klocka ett steg
+    public void  step() { 
+       int time = time ++ ;
     }
 
     public boolean isGreen()   {
-    	return false;
+    	if (time<green) {
+    		return true;
+    	}else 
+    		return false;
+    		
     	// Returnerar true om time<green, annars false
     }
 
