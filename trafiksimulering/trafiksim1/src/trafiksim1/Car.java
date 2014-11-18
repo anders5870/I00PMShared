25a3bcba442deda3ced5f976384a56d026da7a50
Anders Ålander <anders5870@yahoo.se>
2014-11-18 11:09:10 +0100

    la till trafiksim1

---
+++ trafiksimulering/trafiksim1/src/trafiksim1/Car.java
@@ -0,0 +1,11 @@
+package trafiksim1;
+
+public class Car {
+    private int bornTime;
+    private int dest; // 1 för rakt fram, 2 för vänstersväng
+
+    // konstruktor och get-metoder
+    ...
+
+    public String toString() {...}
+}
