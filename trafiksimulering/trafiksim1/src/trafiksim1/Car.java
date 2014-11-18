eb54bc7ee61f0e1386ee78e0afe43d50c6d7611a
anders5870 <anders5870@yahoo.se>
2014-11-18 23:41:45 +0100

    destDistribution is passed to the Car constructor, which in turn use it to decide upon its destination

--- trafiksimulering/trafiksim1/src/trafiksim1/Car.java
+++ trafiksimulering/trafiksim1/src/trafiksim1/Car.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Car {
     private int bornTime;
     private int destination; // 1 f�r rakt fram, 2 f�r v�nstersv�ng
@@ -5,11 +7,23 @@ public class Car {
      
     //konstruktor
     //sätter borntime till bt och slumpar fram om bilen ska till vänster eller höger 
-    public Car(int bornTime, int destination){
+    public Car(int bornTime, int destDistribution){
     	this.bornTime = bornTime;
-    	this.destination = destination;	
+    	this.destination = (destDistribution  < randInt(0,100)) ?  1 : 0;
     }
     
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
     
     // get-metod borntime 
     public int getBornTime(){
--- trafiksimulering/trafiksim1/src/trafiksim1/TrafficSystem.java
+++ trafiksimulering/trafiksim1/src/trafiksim1/TrafficSystem.java
@@ -16,18 +16,7 @@ public class TrafficSystem {
     private Light s1;
     private Light s2;
 
-    public static int randInt(int min, int max) {
-
-        // NOTE: Usually this should be a field rather than a method
-        // variable so that it is not re-seeded every call.
-        Random rand = new Random();
-
-        // nextInt is normally exclusive of the top value,
-        // so add 1 to make it inclusive
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-
-        return randomNum;
-    }  
+  
     
     // Diverse attribut för simuleringsparametrar (ankomstintensiteter,
     // destinationer...)
@@ -75,6 +64,8 @@ public class TrafficSystem {
     		    		break;
     			  case "time2":
     		    		s2.setTime(Integer.parseInt(value));
+    		    		break;
+    		    		
     		    		default:
     		    			System.out.print("properties file is missing!!!");
     			  }
