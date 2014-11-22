dc80c0fe2b3d92cee408071d1127d5cc2e9b9446
anders5870 <anders5870@yahoo.se>
2014-11-22 13:23:45 +0100

    "simple user interface made"

--- trafiksimulering/trafiksim1/src/trafiksim1/Simulation.java
+++ trafiksimulering/trafiksim1/src/trafiksim1/Simulation.java
@@ -1,23 +1,53 @@
 package trafiksim1;
 
+import java.util.Scanner;
+
 public class Simulation {
 	  public static void main(String [] args) {
 			// Skapar ett TrafficSystem
 			// Utför stegningen, anropar utskriftsmetoder
 
 			TrafficSystem system = new TrafficSystem();
+			Scanner sc = new Scanner(System.in);
+			int  choice = -1;
+			while(choice != 0) {
+				System.out.println("Hello! Type:");
+				System.out.println("1. to step the system once");
+				System.out.println("2. to step through the whole system once");
+				System.out.println("3. to step through the system a number of times by your choice");
+				System.out.println("4. to print the system");
+				System.out.println("5. to print the statistics collected so far");
+				System.out.println("0. to exit the program!");
+				System.out.print("> ");
+				choice = sc.nextInt();
+				switch(choice){
+					case 1:
+						system.step();
+						break;
+					case 2:
+						for (int i = 0; system.sysLen() > i; i++) system.step();
+						break;
+					case 3:
+						for (int i = (sc.nextInt()); i > 0; i--) system.step();
+						break;
+					case 4:
+						system.print();
+						break;
+					case 5:
+						system.printStatistics();
+						break;	
+					case 0:
+						System.out.println("Goodbye!");
+						break;
+					default:
+						System.out.println("Could not parse choice, please try again");
+				}
 
-			//X how to know when a complete iteration is done?
-			for(int i = 0; system.sysLen()*10 > i; i++){
-				system.step();
 			}
-			
-			//X how to show the current time of the system?
-			//X how to show how many cars are in the wrong lane?
-			//X how to know how many cars have passed the system?
-			system.printStatistics();
-			//X how to show where there are cars in the system?
-			system.print();
 
-		    }
+			sc.close();
+	
+
+
+	  	}
 }
